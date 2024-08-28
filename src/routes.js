78a617dd27c7c49550329af02411ab8ffef31216
naleksica <126725041+naleksica@users.noreply.github.com>
2024-08-28 03:59:02 -0700

    files

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict';
+
+  angular.module('MenuApp')
+  .config(RoutesConfig);
+
+  RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+    .state('home', {
+      url: '/',
+      templateUrl: 'src/menuapp/templates/home.template.html'
+    })
+
+    .state('categories', {
+      url: '/categories',
+      templateUrl: 'src/menuapp/templates/categories.template.html',
+      controller: 'CategoriesController as catCtrl',
+      resolve: {
+        items: ['MenuDataService', function(MenuDataService) {
+          return MenuDataService.getAllCategories();
+        }]
+      }
+    })
+
+    .state('items', {
+      url: '/items/{categoryShortName}',
+      templateUrl: 'src/menuapp/templates/items.template.html',
+      controller: 'ItemsController as itemsCtrl',
+      resolve: {
+        items: ['$stateParams', 'MenuDataService',
+          function($stateParams, MenuDataService) {
+            return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+          }]
+      }
+    });
+  }
+})();
